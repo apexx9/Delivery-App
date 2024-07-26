09ef60ad1e66950bcce1c2a2efca6a1a12ce66f4
Aaron Nartey <77204807+devnartey@users.noreply.github.com>
2024-07-26 16:20:52 -0700

    Prettier setup

---
+++ src/components/splashScreen/splashScreen.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SplashScreen = () => {
+  //this is the splash screen component for the app
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+});
